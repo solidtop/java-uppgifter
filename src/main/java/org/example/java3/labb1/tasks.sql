da29769bb233a22763ea8b743954437697d0919c
Axel Asp <solidtop.asp@gmail.com>
2023-11-09 08:59:18 +0100

    Uncomment

--- src/main/java/org/example/java3/labb1/tasks.sql
+++ src/main/java/org/example/java3/labb1/tasks.sql
@@ -1,50 +1,50 @@
 USE laboration1;
 
--- Task 1
-# CREATE TABLE IF NOT EXISTS successful_mission
-# AS
-# SELECT *
-# FROM moon_mission
-# WHERE outcome = 'Successful';
-#
-# -- Task 2
-# START TRANSACTION;
-# ALTER TABLE successful_mission
-#     MODIFY mission_id INT AUTO_INCREMENT PRIMARY KEY;
-# COMMIT;
+Task 1
+CREATE TABLE IF NOT EXISTS successful_mission
+AS
+SELECT *
+FROM moon_mission
+WHERE outcome = 'Successful';
 
--- Task 3
-# START TRANSACTION;
-# SELECT REPLACE(operator, ' ', '')
-# FROM successful_mission;
-# COMMIT;
+-- Task 2
+START TRANSACTION;
+ALTER TABLE successful_mission
+    MODIFY mission_id INT AUTO_INCREMENT PRIMARY KEY;
+COMMIT;
 
--- Task 4
-# START TRANSACTION;
-# DELETE
-# FROM successful_mission
-# WHERE launch_date >= '2010-01-01';
-# COMMIT;
+Task 3
+START TRANSACTION;
+SELECT REPLACE(operator, ' ', '')
+FROM successful_mission;
+COMMIT;
 
--- Task 5
-# SELECT *,
-#        CONCAT(first_name, ' ', last_name)                            AS name,
-#        IF(SUBSTR(ssn, LENGTH(ssn) - 1, 1) % 2 = 0, 'female', 'male') AS gender
-# FROM account;
+Task 4
+START TRANSACTION;
+DELETE
+FROM successful_mission
+WHERE launch_date >= '2010-01-01';
+COMMIT;
 
--- Task 6
-# START TRANSACTION;
-# DELETE
-# FROM account
-# WHERE SUBSTR(ssn, LENGTH(ssn) - 1, 1) % 2 = 0
-#   AND YEAR(STR_TO_DATE(CONCAT('19', SUBSTR(ssn, 1, 2), '-', SUBSTR(ssn, 3, 2), '-', SUBSTR(ssn, 5, 2)), '%Y-%m-%d')) <
-#       1970;
-# COMMIT;
+Task 5
+SELECT *,
+       CONCAT(first_name, ' ', last_name)                            AS name,
+       IF(SUBSTR(ssn, LENGTH(ssn) - 1, 1) % 2 = 0, 'female', 'male') AS gender
+FROM account;
 
--- Task 7
-# SELECT *
-#
-# FROM account
+Task 6
+START TRANSACTION;
+DELETE
+FROM account
+WHERE SUBSTR(ssn, LENGTH(ssn) - 1, 1) % 2 = 0
+  AND YEAR(STR_TO_DATE(CONCAT('19', SUBSTR(ssn, 1, 2), '-', SUBSTR(ssn, 3, 2), '-', SUBSTR(ssn, 5, 2)), '%Y-%m-%d')) <
+      1970;
+COMMIT;
+
+Task 7
+SELECT *
+
+FROM account
 
 SELECT 'male'                             AS gender,
        AVG(YEAR(CURDATE()) -
